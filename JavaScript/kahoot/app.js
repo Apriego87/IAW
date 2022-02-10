5c6793397356c543859ce1142a3e51aff9450801
Apriego87 <antoniopriego22@gmail.com>
2022-02-10 03:03:14 +0100

    Kahoot terminado lesgo

--- JavaScript/kahoot/app.js
+++ JavaScript/kahoot/app.js
@@ -8,8 +8,7 @@ bien = 0
 
 mal = 0
 
-
-function cambiarPregunta() {
+function cambiarPregunta(clickedDiv) {
     if (contador == 0) {
         p = document.querySelector(".p")
         p.innerHTML = preg[0]
@@ -25,6 +24,7 @@ function cambiarPregunta() {
         contador++
     }
     else if (contador == 1) {
+        divClicao = clickedDiv
         preg = ["<h3>¿En qué continente se encuentra Barbados?</h3>"]
         resp = ["América", "Oceanía", "Asia", "África"]
 
@@ -40,9 +40,21 @@ function cambiarPregunta() {
         r4 = document.querySelector(".resp4")
         r4.innerHTML = resp[3]
 
+        if (divClicao.className == 'r3') {
+            bien++
+            unaBien = document.querySelector(".b")
+            unaBien.innerHTML = "<p>Bien: </p>" + bien
+        }
+        else if (divClicao.className != 'r3') {
+            mal++
+            unamal = document.querySelector(".m")
+            unamal.innerHTML = "<p>Mal: </p>" + mal
+        }
+
         contador++
     }
     else if (contador == 2) {
+        divClicao = clickedDiv
         preg = ["<h3>¿Detrás de qué planeta se encuentra el Cinturón de Asteroides de la Vía Láctea?</h3>"]
         resp = ["Saturno", "Marte", "Júpiter", "Mercurio"]
 
@@ -57,5 +69,35 @@ function cambiarPregunta() {
         r3.innerHTML = resp[2]
         r4 = document.querySelector(".resp4")
         r4.innerHTML = resp[3]
+
+        if (divClicao.className == 'r1') {
+            bien++
+            unaBien = document.querySelector(".b")
+            unaBien.innerHTML = "<p>Bien: </p>" + bien
+        }
+        else if (divClicao.className != 'r1') {
+            mal++
+            unamal = document.querySelector(".m")
+            unamal.innerHTML = "<p>Mal: </p>" + mal
+        }
+
+        contador++
+    }
+    
+    else if (contador == 3) {
+        divClicao = clickedDiv
+
+        if (divClicao.className == 'r2') {
+            bien++
+            unaBien = document.querySelector(".b")
+            unaBien.innerHTML = "<p>Bien: </p>" + bien
+        }
+        else if (divClicao.className != 'r2') {
+            mal++
+            unamal = document.querySelector(".m")
+            unamal.innerHTML = "<p>Mal: </p>" + mal
+        }
+        final = document.querySelector(".e")
+        final.innerHTML = "<h4>Resultado Final --> Bien: " + bien + " |  Mal: " + mal + "</h4>"
     }
 }
