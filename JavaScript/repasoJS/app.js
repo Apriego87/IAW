034b6af12331665e987628f743dd80cc75deb278
Apriego87 <antoniopriego22@gmail.com>
2022-02-21 17:11:54 +0100

    repasoJS

---
+++ JavaScript/repasoJS/app.js
@@ -0,0 +1,6 @@
+clase = ['sanchez','adrian3','petre','borjaB','miguel','ruben','kevin','raul','borjaM','hugo','pablo', 'nuria','dayana','adrian1','miquel']
+
+function cambiarNombre(this){
+    prueba = document.querySelector('.alguien')
+    prueba.innerHTML = clase[0]
+}
